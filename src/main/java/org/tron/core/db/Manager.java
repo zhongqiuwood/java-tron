7464c02e13f79f17330c271df0596969e747f004
wubin1 <wubin1@tron.network>
2019-05-04 14:52:52 +0800

    add failed deferred transaction on chain

--- src/main/java/org/tron/core/db/Manager.java
+++ src/main/java/org/tron/core/db/Manager.java
@@ -28,7 +28,7 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.Executors; 
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ScheduledExecutorService;
