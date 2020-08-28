f4fec8bb21a37a254dd96ba75167234e4ee838f9
Matt <Matt>
2020-08-28 19:50:21 +0800

    replay and cache

--- common/src/main/java/org/tron/common/parameter/CommonParameter.java
+++ common/src/main/java/org/tron/common/parameter/CommonParameter.java
@@ -411,7 +411,7 @@ public class CommonParameter {
   @Getter
   @Setter
   @Parameter(names = {"--replay"})
-  private boolean replay;
+  private boolean replay = false;
 
   private static double calcMaxTimeRatio() {
     //return max(2.0, min(5.0, 5 * 4.0 / max(Runtime.getRuntime().availableProcessors(), 1)));
