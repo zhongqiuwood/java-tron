6df2e1f6a8a165eb331c324ce5192e1d0eced474
Timothy <57668523+timothyckw@users.noreply.github.com>
2020-09-01 17:58:53 +0800

    add output file for benchmark

--- framework/src/test/java/org/tron/common/runtime/vm/RefineBenchMarkTest.java
+++ framework/src/test/java/org/tron/common/runtime/vm/RefineBenchMarkTest.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
 import org.tron.common.parameter.CommonParameter;
 import org.tron.common.runtime.InternalTransaction;
 import org.tron.core.config.args.Args;
@@ -13,7 +14,7 @@ import org.tron.core.vm.program.Program;
 import org.tron.core.vm.program.invoke.ProgramInvokeMockImpl;
 import org.tron.protos.Protocol.Transaction;
 
-import java.io.File;
+import java.io.*;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -68,14 +69,17 @@ public class RefineBenchMarkTest {
         //xor
         opMap.put((byte)0x18, new byte[]{0x5b, 0x60, 0x01, 0x60, 0x01, 0x18, 0x50, 0x60, 0x00, 0x56});
 
+        long count = 500000;
         long billion = 1000000000L;
         long billion5 = 5000000000L;
         long billion10 = 10000000000L;
         long billion20 = 20000000000L;
         long billion40 = 40000000000L;
-        long count = 500000;
 
+        String msg;
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("OriginalBenchMark.txt")) ;
         for (Map.Entry<Byte, byte[]> entry : opMap.entrySet()) {
+            bufferedWriter.write("-------->" + OpCode.code(entry.getKey()) + "\n");
             for(int i = 1; i <= 10; i++) {
                 VM vm = new VM();
                 vm.targetOp = entry.getKey();
@@ -91,11 +95,12 @@ public class RefineBenchMarkTest {
                 double time = ((double) vm.timeAll) / vm.count;
                 //System.out.println(String.format("\"%s(0x%02x)\"\t\t\t\t%d\t\t\t\t%f",
                 //       OpCode.code(entry.getKey()),entry.getKey(), vm.count,time));
-                System.out.println(String.format("%d,%s(0x%02x),%d,%d,%f",
+                msg = String.format("%d,%s(0x%02x),%d,%d,%f",
                         i, OpCode.code(entry.getKey()), entry.getKey(), vm.timeAll, vm.count,
-                        time));
+                        time);
+                bufferedWriter.write(msg + "\n");
             }
-            System.out.println("-----");
         }
+        bufferedWriter.close();
     }
 }
